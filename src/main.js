9c8c2e84a1fd66edd050aabcd7c059a906adcb27
Lazar Dejanovic <lazzar99@gmail.com>
2022-08-13 17:46:49 +0200

    minor changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import VueSocketIO from 'vue-socket.io';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import store from './store'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.use(new VueSocketIO({
+  debug: false,
+  connection: 'ws://127.0.0.1:8081',
+  vuex: {
+    store,
+    actionPrefix: 'socket_',
+  }
+}));
 
 Vue.config.productionTip = false
 
